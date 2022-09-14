8c635962528e221aa5baf25dccb87cb4a309551a
aubemond <cielomond@naver.com>
2022-09-14 17:49:15 +0900

    [update] 주문목록 페이지 javascript 작업중. 더보기 버튼 기능.

--- js/shopping_list.js
+++ js/shopping_list.js
@@ -14,3 +14,28 @@ $('.search-time-box div').click(function(){
   $(this).addClass('current');
   $("#"+tab_id).addClass('current');
 })
+
+
+
+
+
+  $(".date-item").slice(0, 2).show(); // select the first two
+  $(".viewmore-btn").click(function(e){ // click event for load more
+  e.preventDefault();
+  $(".date-item:hidden").slice(0, 2).show(); // select next 2 hidden divs and show them
+  if($(".date-item:hidden").length == 0){ // check if any hidden divs still exist
+  // alert("마지막 페이지입니다."); 
+  // alert if there are none left
+  $(".viewmore-btn-section").addClass('remove-btn');
+  }
+});
+  $(".date-item-d").slice(0, 3).show(); // select the first two
+  $(".viewmore-btn").click(function(e){ // click event for load more
+  e.preventDefault();
+  $(".date-item-d:hidden").slice(0, 3).show(); // select next 2 hidden divs and show them
+  if($(".date-item-d:hidden").length == 0){ // check if any hidden divs still exist
+  // alert("마지막 페이지입니다."); 
+  // alert if there are none left
+  $(".viewmore-btn-section2").addClass('remove-btn');
+  }
+});
