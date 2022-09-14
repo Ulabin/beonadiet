c78d91cbfdcd6e2e821601ff4af06b15c9305970
KIM MINJAE <65687521+KIMMINJAE-Great@users.noreply.github.com>
2022-09-14 15:42:29 +0900

    [update]마이페이지 사이드바  기능추가
    사이드바 목록 hover시 폰트 굵기를 변경시켜 무엇을 선택하고 있는지 확실하게 알게 합니다.

---
+++ view_kmj/javascript/mypage.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $('.my-page-list button').hover(function(){
+    $(this).css("font-weight", "bold");
+    
+    
+  }, function () {
+    $(this).css("font-weight",400);
+    
+  });
+})
