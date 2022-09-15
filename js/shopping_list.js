bf175fa7dbca2d7cf75cd49b70238d4c74306974
aubemond <cielomond@naver.com>
2022-09-15 12:24:29 +0900

    [update] 주문목록 페이지 javascript 추가. 더보기 버튼 기능 추가. 주문취소, 교환/반품신청 버튼 기능 추가.

--- js/shopping_list.js
+++ js/shopping_list.js
@@ -15,27 +15,50 @@ $('.search-time-box div').click(function(){
   $("#"+tab_id).addClass('current');
 })
 
-
-
-
-
-  $(".date-item").slice(0, 2).show(); // select the first two
-  $(".viewmore-btn").click(function(e){ // click event for load more
-  e.preventDefault();
-  $(".date-item:hidden").slice(0, 2).show(); // select next 2 hidden divs and show them
-  if($(".date-item:hidden").length == 0){ // check if any hidden divs still exist
-  // alert("마지막 페이지입니다."); 
-  // alert if there are none left
+  $(".date-item").slice(0, 2).show(); 
+  $(".viewmore-btn").click(function(e){ 
+  $(".date-item:hidden").slice(0, 2).show(); 
+  if($(".date-item:hidden").length == 0){ 
   $(".viewmore-btn-section").addClass('remove-btn');
   }
 });
-  $(".date-item-d").slice(0, 3).show(); // select the first two
-  $(".viewmore-btn").click(function(e){ // click event for load more
-  e.preventDefault();
-  $(".date-item-d:hidden").slice(0, 3).show(); // select next 2 hidden divs and show them
-  if($(".date-item-d:hidden").length == 0){ // check if any hidden divs still exist
-  // alert("마지막 페이지입니다."); 
-  // alert if there are none left
+
+  $(".date-item-2").slice(0, 2).show(); 
+  $(".viewmore-btn").click(function(e){ 
+  $(".date-item-2:hidden").slice(0, 2).show();
+  if($(".date-item-2:hidden").length == 0){ 
   $(".viewmore-btn-section2").addClass('remove-btn');
+  }
+});
+
+  $(".date-item-3").slice(0, 2).show(); 
+  $(".viewmore-btn").click(function(e){ 
+  $(".date-item-3:hidden").slice(0, 2).show();
+  if($(".date-item-3:hidden").length == 0){ 
+  $(".viewmore-btn-section3").addClass('remove-btn');
+  }
+});
+
+  $(".date-item-4").slice(0, 2).show(); 
+  $(".viewmore-btn").click(function(e){ 
+  $(".date-item-4:hidden").slice(0, 2).show();
+  if($(".date-item-4:hidden").length == 0){ 
+  $(".viewmore-btn-section4").addClass('remove-btn');
+  }
+});
+
+$('.pay-cancel').click(function(){
+  if(!confirm("주문 취소하시겠습니까?")){
+    alert('취소버튼을 눌렀습니다.'); 
+  }else{
+    alert('주문이 취소되었습니다.');
+  }
+});
+
+$('.start-cancel').click(function(){
+  if(!confirm('1:1문의게시판을 통해 교환/반품이 가능합니다.\n확인버튼을 누르시면 1:1문의게시판으로 이동합니다.')){
+    alert('취소버튼을 눌렀습니다.'); 
+  }else{
+    alert('1:1 문의게시판 이동');
   }
 });
