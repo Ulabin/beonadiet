dbed992a478dbb3216ec62c22446cb25d1b3229a
labin2596-admin <labin2596@gmail.com>
2022-09-17 04:54:28 +0900

    [initial] 스프링부트 프로젝트 issue #24 반드시 읽고 작업진행

---
+++ beonadiet/src/main/java/com/beonadiet/beonadiet/BeonadietApplication.java
@@ -0,0 +1,13 @@
+package com.beonadiet.beonadiet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeonadietApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeonadietApplication.class, args);
+	}
+
+}
