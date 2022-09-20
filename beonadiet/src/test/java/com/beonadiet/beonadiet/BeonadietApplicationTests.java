300e1cd04a23aa4c50ec13edea6a0fd3f967e1a5
aubemond <cielomond@naver.com>
2022-09-20 10:59:53 +0900

    [update] springboot 추가.

---
+++ beonadiet/src/test/java/com/beonadiet/beonadiet/BeonadietApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beonadiet.beonadiet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeonadietApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
