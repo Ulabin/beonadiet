d77745fd0f125a040a5dfd55f475f1b5aa0e7ab0
KIM MINJAE <minjae2717@gmail.com>
2022-10-11 23:04:29 +0900

    [update]나의게시글 수정
    나의 게시글 방식을 수정하였습니다.

--- beonadiet/src/main/resources/static/js/mypage.js
+++ beonadiet/src/main/resources/static/js/mypage.js
@@ -10,25 +10,25 @@ $(document).ready(function () {
 
   
 })
-function getMyPosts() {
-  console.log("클릭")
+// function getMyPosts() {
+//   console.log("클릭")
   
-    /*<![CDATA[*/
-    $.ajax({
-      type: "GET",
-      url: "mypage/mypost",
-    })
-      .done(function (result) {
-        $('.my-page-detail').empty();
-        $('.my-page-detail').html(result)
-      })
-      .fail(function (jqXHR) {
-        console.log(jqXHR);
-        console.log("에러");
-      })
-      .always(function () {
-        console.log("요청, 응답 결과에 상관없이, 이건 항상 실행됩니다.");
-      })
-    /*]]*/
-  }
+//     /*<![CDATA[*/
+//     $.ajax({
+//       type: "GET",
+//       url: "mypage/mypost",
+//     })
+//       .done(function (result) {
+//         $('.my-page-detail').empty();
+//         $('.my-page-detail').html(result)
+//       })
+//       .fail(function (jqXHR) {
+//         console.log(jqXHR);
+//         console.log("에러");
+//       })
+//       .always(function () {
+//         console.log("요청, 응답 결과에 상관없이, 이건 항상 실행됩니다.");
+//       })
+//     /*]]*/
+//   }
 
